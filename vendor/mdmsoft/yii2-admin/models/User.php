30aa9cb140b9c07109d00b257d3b4838a5c82acd
igorbakal <igor.baka@gmail.com>
2016-11-04 14:21:45 +0200

    v 3

---
+++ vendor/mdmsoft/yii2-admin/models/User.php
@@ -0,0 +1,276 @@
+<?php
+
+namespace mdm\admin\models;
+
+use Yii;
+use yii\base\NotSupportedException;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
+use mdm\admin\components\Configs;
+
+/**
+ * User model
+ *
+ * @property integer $id
+ * @property string $username
+ * @property string $password_hash
+ * @property string $password_reset_token
+ * @property string email_confirm_token
+ * @property string $email
+ * @property string $notification_type
+ * @property string $auth_key
+ * @property integer $status
+ * @property integer $created_at
+ * @property integer $updated_at
+ * @property string $password write-only password
+ *
+ * @property UserProfile $profile
+ */
+class User extends ActiveRecord implements IdentityInterface
+{
+    const STATUS_INACTIVE = 0;
+    const STATUS_ACTIVE = 10;
+
+    const SCENARIO_PROFILE = 'profile';
+
+    public $types = [
+        'email' => 'email',
+        'browser' => 'browser',
+    ];
+
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return Configs::instance()->userTable;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::className(),
+        ];
+    }
+
+    public function scenarios()
+    {
+        return [
+            self::SCENARIO_DEFAULT => ['username', 'email', 'status', 'typesArray'],
+            self::SCENARIO_PROFILE => ['email', 'typesArray'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['username'], 'string', 'max' => 32],
+            [['email'], 'string', 'max' => 255],
+            ['status', 'default', 'value' => self::STATUS_ACTIVE],
+            ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_INACTIVE]],
+            ['typesArray', 'default', 'value' => reset($this->types)],
+            ['typesArray', 'in', 'range' => $this->types, 'allowArray' => true],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function findIdentity($id)
+    {
+        return static::findOne(['id' => $id, 'status' => self::STATUS_ACTIVE]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
+    }
+
+    /**
+     * Finds user by username
+     *
+     * @param string $username
+     * @return static|null
+     */
+    public static function findByUsername($username)
+    {
+        return static::findOne(['username' => $username, 'status' => self::STATUS_ACTIVE]);
+    }
+
+    /**
+     * Finds user by password reset token
+     *
+     * @param string $token password reset token
+     * @return static|null
+     */
+    public static function findByPasswordResetToken($token)
+    {
+        if (!static::isPasswordResetTokenValid($token)) {
+            return null;
+        }
+
+        return static::findOne([
+                'password_reset_token' => $token,
+                'status' => self::STATUS_ACTIVE,
+        ]);
+    }
+
+    /**
+     * Finds out if password reset token is valid
+     *
+     * @param string $token password reset token
+     * @return boolean
+     */
+    public static function isPasswordResetTokenValid($token)
+    {
+        if (empty($token)) {
+            return false;
+        }
+        $expire = Yii::$app->params['user.passwordResetTokenExpire'];
+        $parts = explode('_', $token);
+        $timestamp = (int) end($parts);
+        return $timestamp + $expire >= time();
+    }
+
+    /**
+     * @param string $email_confirm_token
+     * @return static|null
+     */
+    public static function findByEmailConfirmToken($email_confirm_token)
+    {
+        return static::findOne(['email_confirm_token' => $email_confirm_token, 'status' => self::STATUS_INACTIVE]);
+    }
+
+    /**
+     * Generates email confirmation token
+     */
+    public function generateEmailConfirmToken()
+    {
+        $this->email_confirm_token = Yii::$app->security->generateRandomString();
+    }
+
+    /**
+     * Removes email confirmation token
+     */
+    public function removeEmailConfirmToken()
+    {
+        $this->email_confirm_token = null;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getId()
+    {
+        return $this->getPrimaryKey();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAuthKey()
+    {
+        return $this->auth_key;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function validateAuthKey($authKey)
+    {
+        return $this->getAuthKey() === $authKey;
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return boolean if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return Yii::$app->security->validatePassword($password, $this->password_hash);
+    }
+
+    /**
+     * Generates password hash from password and sets it to the model
+     *
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password_hash = Yii::$app->security->generatePasswordHash($password);
+    }
+
+    /**
+     * Generates "remember me" authentication key
+     */
+    public function generateAuthKey()
+    {
+        $this->auth_key = Yii::$app->security->generateRandomString();
+    }
+
+    /**
+     * Generates new password reset token
+     */
+    public function generatePasswordResetToken()
+    {
+        $this->password_reset_token = Yii::$app->security->generateRandomString() . '_' . time();
+    }
+
+    /**
+     * Removes password reset token
+     */
+    public function removePasswordResetToken()
+    {
+        $this->password_reset_token = null;
+    }
+
+    /**
+     * Serialize array
+     * @param $value
+     */
+    public function setTypesArray($value)
+    {
+        $this->notification_type = serialize($value);
+    }
+    /**
+     * Unserialize array
+     * @return mixed
+     */
+    public function getTypesArray()
+    {
+        return unserialize($this->notification_type);
+    }
+    /**
+     * Return notification array divided by comma
+     * @return mixed
+     */
+    public function getTypesString()
+    {
+        return implode(',', $this->typesArray);
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'created_at' => 'Создан',
+            'updated_at' => 'Обновлён',
+            'username' => 'Логин',
+            'email' => 'E-mail',
+            'status' => 'Статус',
+        ];
+    }
+}
---
+++ vendor/mdmsoft/yii2-admin/views/user/view.php
@@ -0,0 +1,61 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+use mdm\admin\components\Helper;
+
+/* @var $this yii\web\View */
+/* @var $model mdm\admin\models\User */
+
+$this->title = $model->username;
+$this->params['breadcrumbs'][] = ['label' => Yii::t('rbac-admin', 'Users'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+
+$controllerId = $this->context->uniqueId . '/';
+?>
+<div class="user-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?php
+        if ($model->status == 0 && Helper::checkRoute($controllerId . 'activate')) {
+            echo Html::a(Yii::t('rbac-admin', 'Activate'), ['activate', 'id' => $model->id], [
+                'class' => 'btn btn-primary',
+                'data' => [
+                    'confirm' => Yii::t('rbac-admin', 'Are you sure you want to activate this user?'),
+                    'method' => 'post',
+                ],
+            ]);
+        }
+        ?>
+        <?php
+        if (Helper::checkRoute($controllerId . 'delete')) {
+            echo Html::a(Yii::t('rbac-admin', 'Delete'), ['delete', 'id' => $model->id], [
+                'class' => 'btn btn-danger',
+                'data' => [
+                    'confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+                    'method' => 'post',
+                ],
+            ]);
+        }
+        ?>
+        <?= Html::a(Yii::t('rbac-admin', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+    </p>
+
+    <?=
+    DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'username',
+            'email:email',
+            'created_at:date',
+            [
+                'attribute' => 'status',
+                'value' => $model->status == 0 ? 'Неактивный' : 'Активный',
+            ],
+        ],
+    ])
+    ?>
+
+</div>
